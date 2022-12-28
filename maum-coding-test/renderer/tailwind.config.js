06d56f8eb51b1f6920295714d0950fe415469476
hopak-e <a328028@gmail.com>
2022-12-29 00:23:40 +0900

    🎉 next + electron 초기설정

---
+++ maum-coding-test/renderer/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      // use colors only specified
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+      black: colors.black,
+      red: colors.red,
+      pink: "#FF6161",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
