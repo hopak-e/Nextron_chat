e12c5847284a6623326af81c184ffc1a55d70a77
hopak-e <a328028@gmail.com>
2023-01-05 02:58:19 +0900

    🎨 체크 여부에 따라 1:1 대화, 그룹 대화방 생성하도록 수정

--- maum-coding-test/renderer/pages/home.tsx
+++ maum-coding-test/renderer/pages/home.tsx
@@ -2,49 +2,74 @@ import React, { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import useCollection from "../hooks/useCollection";
+
 import Footer from "../components/layout/footer";
 import { collection, getDocs, query, addDoc, where } from "firebase/firestore";
 import { db } from "../firebase";
 import Header from "../components/layout/header";
-import { Lists } from "../types/type";
+import { Lists, UserLists } from "../types/types";
 
 function Home() {
-  //로그인 안돼있으면 로그인창으로 이동 -> useEffect 사용
-  const { data } = useCollection("all-chats", collection(db, "chats"));
   const router = useRouter();
   const [userList, setUserList] = useState<Lists[]>([]);
-  const [chatBtn, setChatBtn] = useState<string>("");
   const [admin, setAdmin] = useState({ adminUid: "", adminName: "" });
+  const [checkList, setCheckList] = useState<UserLists[]>([]);
 
   useEffect(() => {
-    async function getUserList() {
-      const userListRef = collection(db, "users");
-      const q = await query(userListRef);
-      const data = await getDocs(q);
-      const newData = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      setUserList(newData);
-    }
-    getUserList();
-
     const adminUid = localStorage.getItem("uid");
     const adminName = localStorage.getItem("displayName");
     if (!adminUid || !adminName) {
       router.push("/signin");
     } else setAdmin({ adminUid, adminName });
+
+    async function getUserList() {
+      const userListRef = collection(db, "users");
+      const q = await query(userListRef);
+      const data = await getDocs(q);
+      const newData = data.docs.map((doc) => ({
+        displayName: doc.data().displayName,
+        uid: doc.data().uid,
+        id: doc.id,
+      }));
+      setUserList(newData.filter((res) => res.uid !== adminUid));
+    }
+    getUserList();
   }, []);
 
-  const handleCreateChatClick = async (uid: string, displayName: string) => {
-    const users = [
-      { uid, displayName },
-      { uid: admin.adminUid, displayName: admin.adminName },
-    ];
-    const q = query(collection(db, "chats"), where("users", "==", users));
-    const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-      await addDoc(collection(db, "chats"), {
-        users,
+  const handleCreateChatClick = async () => {
+    if (!checkList.length) {
+      alert("채팅 할 상대를 선택해주세요.");
+    } else {
+      const users = [
+        ...checkList,
+        { uid: admin.adminUid, displayName: admin.adminName },
+      ].sort((a, b) => {
+        if (a.uid > b.uid) return 1;
+        if (a.uid < b.uid) return -1;
+        return 0;
       });
+
+      const q = query(collection(db, "chats"), where("users", "==", users));
+      const querySnapshot = await getDocs(q);
+      if (querySnapshot.empty) {
+        const addUser = await addDoc(collection(db, "chats"), {
+          users,
+        });
+        router.push(`/Chat/${addUser.id}`);
+      } else router.push(`/Chat/${querySnapshot.docs[0].id}`);
+    }
+  };
+
+  const handleCheckChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    uid: string,
+    displayName: string
+  ) => {
+    if (checkList.find((res) => res.uid === uid)) {
+      const uncheck = checkList.filter((res) => res.uid !== uid);
+      setCheckList(uncheck);
+    } else {
+      setCheckList([...checkList, { uid, displayName }]);
     }
   };
 
@@ -54,32 +79,29 @@ function Home() {
         <title>UserList</title>
       </Head>
       <div className="h-screen">
-        <Header>유저목록</Header>
-        <Link href="/signin">Sign In</Link>
-        <Link href="/signup">Sign Up</Link>
+        <Header>
+          유저목록
+          <button className="absolute right-2" onClick={handleCreateChatClick}>
+            채팅 생성
+          </button>
+        </Header>
         <ul>
           {userList &&
             userList.map((res) => (
               <li
                 key={res.id}
-                className="flex border-b-2 py-4 overflow-y-auto cursor-pointer"
-                onClick={() => setChatBtn(res.uid)}
+                className="flex items-center border-b-2 h-14 pl-2  overflow-y-auto cursor-pointer"
               >
-                <div className="w-20">{res.displayName}</div>
-                <div>
-                  {chatBtn === res.uid && (
-                    <Link href={`/Chat/${res.id}`}>
-                      <button
-                        className="border-2 rounded-md"
-                        onClick={() =>
-                          handleCreateChatClick(res.uid, res.displayName)
-                        }
-                      >
-                        1:1 대화하기
-                      </button>
-                    </Link>
-                  )}
+                <div className="flex mr-2">
+                  <input
+                    type="checkbox"
+                    className="w-6 h-6 cursor-pointer"
+                    onChange={(e) =>
+                      handleCheckChange(e, res.uid, res.displayName)
+                    }
+                  />
                 </div>
+                <div className="w-20">{res.displayName}</div>
               </li>
             ))}
         </ul>
